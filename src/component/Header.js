338f45ef8a9f11db35b0ee89052087eb72b58fe4
Balram Gupta <109209618+balramg7@users.noreply.github.com>
2023-05-16 15:56:52 +0530

    start work

---
+++ src/component/Header.js
@@ -0,0 +1,34 @@
+import "./Header.css";
+function Header() {
+  return (
+    <div>
+      <div className="kXxyX">
+        <div className="lbjtRV bynpIh">
+          <div id="photo-welcome"></div>
+          <div className="ivgVxv"></div>
+          <div className="BhOHA">
+            <div className="bg-img"></div>
+            <div y="bottom" x="left" className=" caXUyO ivSKJL">
+              <p className="bGnSAG">Arpit &amp; Khushi</p>
+              <p className="iZsRoL">
+                We can't wait to share our special day with you. Help us capture
+                our wedding with Joy.
+              </p>
+            </div>
+          </div>
+          <button type="button" className="iQpztU" fdprocessedid="yj9ecxv">
+            <div className="iCvooE">
+              <svg className="HowYm">
+                <rect width="24" height="2" rx="1"></rect>
+                <rect y="8" width="24" height="2" rx="1"></rect>
+                <rect y="16" width="24" height="2" rx="1"></rect>
+              </svg>
+              <p className="drvxON">MENU</p>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Header;
---
+++ src/component/Main.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import './Main.css';
+
+function Main() {
+  return (
+    <div>
+      <div id="root">
+        <main className="iwnlHO">
+          <Header />
+        </main>
+      </div>
+    </div>
+  );
+}
+export default Main;
